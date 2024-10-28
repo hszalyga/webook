2eb4c20922aa5b26e5a114fc0d3ad4b2d9abb3dc
Hubert <hszalyga@student.wszib.edu.pl>
2024-10-29 00:16:53 +0100

    pierwsze dodawanie listy książek do aplikacji

--- webooks/views.py
+++ webooks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+def all_books(request):
+    found_books = Book.find_all_books()
+    context = {
+        'books': found_books
+    }
+    return render(request, 'book/all_books.html', context)
