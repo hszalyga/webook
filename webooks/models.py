d1c7af04d2041db966110605e6aaeb8e20337066
Hubert <hszalyga@student.wszib.edu.pl>
2024-10-28 23:41:01 +0100

    dodanie bazy danych filmów

--- webooks/models.py
+++ webooks/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import csv
+import datetime
 
 # Create your models here.
+class Book:
+    def __init__(self, book_id, title, authors, original_publication_year, average_rating, rating_count, image_url):
+        self.book_id = book_id
+        self.title = title
+        self.authors = authors
+        self.original_publication_year = original_publication_year
+        self.average_rating = average_rating
+        self.rating_count = rating_count
+        self.image_url = image_url
+
+    @staticmethod
+    def find_all_books():
+        books_list = []
+
+        with open('webooks/migrations/books.csv', r, encoding='utf8') as all_books_file:
+            reader = csv.DictReader(all_books_file, delimiter=',')
+
+            for row in reader:
+                books_list.append(Book(
+                    row['book_id'],
+                    row['title'],
+                    row['authors'],
+                    datetime.datetime.strptime(row['published_date'], '%Y'),
+                    float(row['average_rating']),
+                    int(row['rating_count']),
+                    row['image_url'],
+                ))
+
+        return books_list
