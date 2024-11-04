31034572a4e7362a509cfb8c1cf83b93276e4bc3
Hubert <hszalyga@student.wszib.edu.pl>
2024-11-04 23:01:06 +0100

    Adjustacja wyglądu strony, dodanie przyciskow (z class)

--- webooks/urls.py
+++ webooks/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_books, name='all_books_uls'),
-    path('<book_id>', views.book_details, name='book_details_url'),
-]
+    path('', views.all_books, name='all_books_url'),
+    path('<book_id>', views.book_details, name='book_details_url'),]
