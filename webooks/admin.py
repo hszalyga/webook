89444f4e4ecf7735733f6a34fe53c5656d26ec6b
Hubert <hszalyga@student.wszib.edu.pl>
2024-11-18 20:51:38 +0100

    Dodanie obsługi admin panel dla modelu Book

--- webooks/admin.py
+++ webooks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from webooks.models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_filter = ('average_rating', 'title', 'authors')
+    list_display = ('book_id', 'title', 'authors', 'language_code', 'original_publication_year', 'average_rating', 'ratings_count', 'small_image_url', 'image_url')
 
 # Register your models here.
+admin.site.register(Book, BookAdmin)
--- webooks/models.py
+++ webooks/models.py
@@ -1,18 +1,20 @@
 from django.db import models
-import csv
-import datetime
+from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
 
 # Create your models here.
 class Book(models.Model):
-    book_id = models.IntegerField()
-    title = models.TextField()
-    authors = models.TextField()
+    book_id = models.IntegerField(validators=[MinValueValidator(1)])
+    title = models.CharField(max_length=1000)
+    authors = models.CharField(max_length=1000)
     language_code = models.CharField(max_length=10)
-    original_publication_year = models.IntegerField(default=0)
-    average_rating = models.FloatField()
-    ratings_count = models.IntegerField()
+    original_publication_year = models.IntegerField(default=0, validators=[MaxValueValidator(4)])
+    average_rating = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    ratings_count = models.IntegerField(validators=[MinValueValidator(0)])
     small_image_url = models.URLField()
     image_url = models.URLField()
+
+    def __str__(self):
+        return self.title
 
     # def __init__(self, book_id, title, authors, language, original_publication_year, average_rating, rating_count, small_image_url):
     #     self.book_id = book_id
@@ -24,25 +26,25 @@ class Book(models.Model):
     #     self.rating_count = rating_count
     #     self.image_url = small_image_url
 
-    @staticmethod
-    def find_all_books():
-        books_list = []
-
-        with open('webooks/migrations/books.csv', 'r', encoding='utf8') as all_books_file:
-            reader = csv.DictReader(all_books_file, delimiter=',')
-
-            for row in reader:
-                books_list.append(Book(
-                    book_id=row['book_id'],
-                    title=row['title'],
-                    authors=row['authors'],
-                    language_code=row['language_code'],
-                    original_publication_year=row['original_publication_year'],
-                    average_rating=float(row['average_ratings']),
-                    ratings_count=int(row.get('ratings_count', 0)),
-                    small_image_url=row['small_image_url'],
-                    image_url=row['image_url'],
-                ))
-
-        return books_list
-
+    # @staticmethod
+    # def find_all_books():
+    #     books_list = []
+    #
+    #     with open('webooks/migrations/books.csv', 'r', encoding='utf8') as all_books_file:
+    #         reader = csv.DictReader(all_books_file, delimiter=',')
+    #
+    #         for row in reader:
+    #             books_list.append(Book(
+    #                 book_id=row['book_id'],
+    #                 title=row['title'],
+    #                 authors=row['authors'],
+    #                 language_code=row['language_code'],
+    #                 original_publication_year=row['original_publication_year'],
+    #                 average_rating=float(row['average_ratings']),
+    #                 ratings_count=int(row.get('ratings_count', 0)),
+    #                 small_image_url=row['small_image_url'],
+    #                 image_url=row['image_url'],
+    #             ))
+    #
+    #     return books_list
+    #
