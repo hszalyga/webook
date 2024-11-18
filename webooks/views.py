7bc3e5edda1c35264e008fa2d800d8d28a976bf0
Hubert <hszalyga@student.wszib.edu.pl>
2024-11-18 18:36:58 +0100

    Dodanie aggregation dla danych - średnich

--- webooks/views.py
+++ webooks/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Book
 from django.http import HttpResponseNotFound
+from django.db.models import Avg, Min, Max, Count
 # Create your views here.
 def all_books(request):
     found_books = Book.objects.all() #pobieranie filmów z bazy danych
+    found_books_aggregation = found_books.aggregate(Avg('average_rating'), Min('average_rating'),
+                                                    Max('average_rating'), Count('id'))
+
     context = {
-        'books': found_books
+        'books': found_books,
+        'aggregation_data': found_books_aggregation
     }
     return render(request, 'webooks/all_books.html', context)
 
